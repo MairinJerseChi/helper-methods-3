17ba39d754d6e411e05b2fedfcaa46a2ae098e2f
mairinjerse <mairinjerse@gmail.com>
2023-03-27 16:25:43 +0000

    -added cards

---
+++ db/migrate/20230327155630_add_released_date_to_movies.rb
@@ -0,0 +1,5 @@
+class AddReleasedDateToMovies < ActiveRecord::Migration[6.1]
+  def change
+    add_column :movies, :released_on, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_27_140226) do
+ActiveRecord::Schema.define(version: 2023_03_27_155630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2023_03_27_140226) do
     t.string "image_url"
     t.string "string"
     t.integer "director_id"
+    t.date "released_on"
   end
 
 end
